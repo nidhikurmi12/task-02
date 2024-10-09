1e5dcc9104c20b723d55dd23dd476001a9cf0d58
Nidhi kurmi <115697832+Nidhi-877@users.noreply.github.com>
2024-10-09 17:13:23 +0530

    addedfile

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,9 +6,9 @@ import { userAuthCheck } from "../middlewares/auth/userMiddleware";
 
 const route = Router();
 
+route.get("/verify/:id",verificationController);
 route.post(paths.user.register.Post[0],registerController);
 route.post(paths.user.login.Post[0],loginController);
 route.get(paths.user.dashboard.Get[0],userAuthCheck,dashBoardController);
-route.get("/verify/:id",verificationController);
 
 export default route;
